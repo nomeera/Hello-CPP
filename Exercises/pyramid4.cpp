2ae9a2610d21337354882c82ff30376bb23100dd
nomeera <abdullahnomeer@gmail.com>
2024-02-24 01:58:41 +0200

    add Tips directory

---
+++ Exercises/pyramid4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hight;
+    cout << "Please Enter Pyramid Hight: ";
+    cin >> hight;
+
+    int spaces = hight - 1;
+    int numbers = 1;
+
+    // hight loop
+    for (int i = 0; i <= hight; i++)
+    {
+        // spaces loop
+        for (int j = 0; j <= spaces; j++)
+            cout << " ";
+
+        // numbers loop
+        for (int j = 0; j < numbers; j++)
+        {
+            if (j % 2 != 0)
+                cout << i;
+            else
+                cout << " ";
+        }
+        cout << endl;
+        spaces--;
+        numbers += 2;
+    }
+
+    return 0;
+}
